02c687f70173b8464bbc0410507b43cdcb50dc07
Jukka Villstedt <jukka.villstedt@gmail.com>
2011-09-15 20:59:11 +0300

    natives are not handled by lein deps

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject clojure-lwjgl "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :repositories {"lwjgl" "http://adterrasperaspera.com/lwjgl"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.lwjgl/lwjgl "2.7.1"]
-                 [org.lwjgl/lwjgl-native "2.7.1"]
-                 [matchure "0.10.1"]]
-  :native-path "native2")
+                 [org.lwjgl/lwjgl-native-platform "2.7.1"]
+                 [matchure "0.10.1"]])
