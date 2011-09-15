2f0d0562063eb9ece4e23d4c0e533c97bd9778f2
Jukka Villstedt <jukka.villstedt@gmail.com>
2011-09-15 20:44:00 +0300

    now natives are not cleared by lein deps

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.lwjgl/lwjgl "2.7.1"]
                  [org.lwjgl/lwjgl-native "2.7.1"]
-                 [matchure "0.10.1"]])
+                 [matchure "0.10.1"]]
+  :native-path "native2")
