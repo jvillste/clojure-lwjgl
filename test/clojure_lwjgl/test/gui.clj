ce9726de7ddf72615ec8dd9e00d89675f24e44d0
Jukka Villstedt <jvillste@hotmail.com>
2011-10-05 22:43:05 +0300

    x

--- test/clojure_lwjgl/test/gui.clj
+++ test/clojure_lwjgl/test/gui.clj
@@ -28,14 +28,18 @@
 (deftest call-event-handlers-test-2
   (is (= {:handler-1 3, :handler-2 3, :event-queue {}}
          (dissoc (gui/call-event-handlers {:event-queue (-> (event-queue/create)
-                                                            (event-queue/add {:type :foo :time 2})
-                                                            (event-queue/add {:type :bar :time 1}))
-                                           :event-handlers #{(fn [gui event] (if (= (:time event) 2)
+                                                            (event-queue/add {:type :b :time 2})
+                                                            (event-queue/add {:type :c :time 2})
+                                                            (event-queue/add {:type :a :time 1}))
+                                           :event-handlers #{(fn [gui event] (if (= (:time event) 1)
                                                                                (assoc gui
                                                                                  :event-queue (event-queue/add (:event-queue gui)
-                                                                                                               {:time 3}))
+                                                                                                               {:time 3 :type :d})
+                                                                                 :handler-1 (flatten (concat (vector (:handler-1 gui))
+                                                                                                             (vector (:type event)))))
                                                                                (assoc gui
-                                                                                 :handler-1 (:time event))))
+                                                                                 :handler-1 (flatten (concat (vector (:handler-1 gui))
+                                                                                                             (vector (:type event)))) )))
                                                              (fn [gui event] (assoc gui
                                                                                :handler-2 (:time event)))}})
                  :event-handlers))))
