be2849a46d2fba2fe6ae6d889a26bd876b8ecd8f
Jukka Villstedt <jukka.villstedt@gmail.com>
2011-10-17 00:17:09 +0300

    fbo rendering works

--- src/clojure_lwjgl/paint/core.clj
+++ src/clojure_lwjgl/paint/core.clj
@@ -44,7 +44,7 @@
   (let [graphics (texture/get-graphics (:texture-1 paint))]
     (doto graphics
       (.setColor Color/GREEN)
-      (.fillRect 0 0 width height))
+      (.fillRect 0 0 100 100))
     paint))
 
 (defn update-window [paint]
@@ -52,13 +52,13 @@
 
 
 (defn render-texture [paint texture]
-  (GL11/glClearColor 0 0 0 1)
+  (GL11/glClearColor 0 0 0 0)
   (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
   (GL11/glLoadIdentity)
   (GL11/glDisable GL11/GL_LIGHTING)
   (GL11/glDisable GL11/GL_BLEND)
   ;;  (GL11/glTexEnvi GL11/GL_TEXTURE_ENV GL11/GL_TEXTURE_ENV_MODE GL11/GL_MODULATE)
-;;  (GL11/glTexEnvi GL11/GL_TEXTURE_ENV GL11/GL_TEXTURE_ENV_MODE GL11/GL_TEXTURE)
+  ;;  (GL11/glTexEnvi GL11/GL_TEXTURE_ENV GL11/GL_TEXTURE_ENV_MODE GL11/GL_TEXTURE)
 
   (texture/bind texture)
   (draw/draw-quads (:vertex-buffer-id (:quad-buffer paint))
@@ -76,6 +76,11 @@
   (frame-buffer-object/bind-texture (:id (:texture-2 paint)))
 
   (GL11/glViewport 0 0 width height)
+  (GL11/glMatrixMode GL11/GL_PROJECTION)
+  (GL11/glLoadIdentity)
+  (GL11/glOrtho 0 width 0 height -1 1)
+  (GL11/glMatrixMode GL11/GL_MODELVIEW)
+  
   (render-texture paint
                   (:texture-1 paint))
   (frame-buffer-object/bind 0)
