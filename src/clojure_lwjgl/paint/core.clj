9ecdaf7ed79e77b6086851b0a8809b43b35f9d36
Jukka Villstedt <jvillste@hotmail.com>
2011-10-18 07:16:52 +0300

    shaders work

--- src/clojure_lwjgl/paint/core.clj
+++ src/clojure_lwjgl/paint/core.clj
@@ -6,7 +6,8 @@
                            [draw :as draw]
                            [texture :as texture]
                            [texture-coordinate-buffer :as texture-coordinate-buffer]
-                           [frame-buffer-object :as frame-buffer-object]))
+                           [frame-buffer-object :as frame-buffer-object]
+                           [shader :as shader]))
   (:import [java.awt Color Font  RenderingHints]
            [org.lwjgl.opengl GL11]))
 
@@ -31,6 +32,37 @@
     :texture-1 (texture/load (:texture-1 paint))
     :texture-2 (texture/load (:texture-2 paint))))
 
+(def vertex-shader-source "
+void main(){
+    gl_Position = gl_ModelViewProjectionMatrix*gl_Vertex;
+}
+")
+
+(def vertex-shader-2-source "
+void main() {
+  gl_TexCoord[0] = gl_MultiTexCoord0;
+  gl_Position = ftransform();
+}
+")
+
+
+
+
+(def fragment-shader-source "
+void main(){
+    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+}
+")
+
+(def fragment-shader-2-source "
+uniform sampler2D tex;
+
+void main() {
+	vec4 color = texture2D(tex,gl_TexCoord[0].st);
+	gl_FragColor = color * vec4(0.5, 0.5, 0.5, 1.0);;
+}
+")
+
 (defn create-paint []
   {:window (window/create)
    :quad-buffer (quad-buffer/create)
@@ -38,13 +70,15 @@
    :texture-coordinate-buffer (texture-coordinate-buffer/create)
    :texture-1 (texture/create width height)
    :texture-2 (texture/create width height)
-   :frame-buffer-object (frame-buffer-object/create)})
+   :frame-buffer-object (frame-buffer-object/create)
+   :shader-program (shader/compile-program vertex-shader-2-source
+                                           fragment-shader-2-source)})
 
 (defn draw-texture [paint]
   (let [graphics (texture/get-graphics (:texture-1 paint))]
     (doto graphics
       (.setColor Color/GREEN)
-      (.fillRect 0 0 100 100))
+      (.fillRect 0 0 200 100))
     paint))
 
 (defn update-window [paint]
@@ -54,9 +88,13 @@
 (defn render-texture [paint texture]
   (GL11/glClearColor 0 0 0 0)
   (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
+  (GL11/glMatrixMode GL11/GL_MODELVIEW)
   (GL11/glLoadIdentity)
-  (GL11/glDisable GL11/GL_LIGHTING)
-  (GL11/glDisable GL11/GL_BLEND)
+
+  (shader/enable-program (:shader-program paint))
+  
+  ;;  (GL11/glDisable GL11/GL_LIGHTING)
+  ;;  (GL11/glDisable GL11/GL_BLEND)
   ;;  (GL11/glTexEnvi GL11/GL_TEXTURE_ENV GL11/GL_TEXTURE_ENV_MODE GL11/GL_MODULATE)
   ;;  (GL11/glTexEnvi GL11/GL_TEXTURE_ENV GL11/GL_TEXTURE_ENV_MODE GL11/GL_TEXTURE)
 
@@ -64,25 +102,33 @@
   (draw/draw-quads (:vertex-buffer-id (:quad-buffer paint))
                    (:buffer-id (:texture-coordinate-buffer paint))
                    (:index-buffer-id (:quad-list paint))
-                   1))
+                   1)
+
+  (shader/disable-program))
 
 (defn render [paint]
   (render-texture paint
-                  (:texture-2 paint))
+                  (:texture-1 paint))
   paint)
 
 (defn render-to-texture [paint]
   (frame-buffer-object/bind (:frame-buffer-object paint))
   (frame-buffer-object/bind-texture (:id (:texture-2 paint)))
 
+  (GL11/glPushAttrib GL11/GL_VIEWPORT_BIT)
   (GL11/glViewport 0 0 width height)
   (GL11/glMatrixMode GL11/GL_PROJECTION)
+  (GL11/glPushMatrix)
   (GL11/glLoadIdentity)
   (GL11/glOrtho 0 width 0 height -1 1)
-  (GL11/glMatrixMode GL11/GL_MODELVIEW)
-  
+
   (render-texture paint
                   (:texture-1 paint))
+
+  (GL11/glMatrixMode GL11/GL_PROJECTION)
+  (GL11/glPopMatrix)
+  (GL11/glPopAttrib)
+
   (frame-buffer-object/bind 0)
   paint)
 
---
+++ src/clojure_lwjgl/shader.clj
@@ -0,0 +1,44 @@
+(ns clojure-lwjgl.shader
+  (:import [org.lwjgl.opengl GL11 ARBShaderObjects ARBVertexShader ARBFragmentShader]
+           [org.lwjgl BufferUtils]))
+
+(defn compile-errors [shader-id]
+  (ARBShaderObjects/glGetInfoLogARB shader-id 1000))
+
+(defn compile-shader [shader-id source]
+  (ARBShaderObjects/glShaderSourceARB shader-id source)
+  (ARBShaderObjects/glCompileShaderARB shader-id)
+  (when (> (count (compile-errors shader-id))
+           0)
+    (throw (Exception. (compile-errors shader-id)))))
+
+(defn create-vertex-shader []
+  (ARBShaderObjects/glCreateShaderObjectARB ARBVertexShader/GL_VERTEX_SHADER_ARB))
+
+(defn create-fragment-shader []
+  (ARBShaderObjects/glCreateShaderObjectARB ARBFragmentShader/GL_FRAGMENT_SHADER_ARB))
+
+(defn create-program [vertex-shader-id fragment-shader-id]
+  (let [program-id (ARBShaderObjects/glCreateProgramObjectARB)]
+    (ARBShaderObjects/glAttachObjectARB program-id vertex-shader-id)
+    (ARBShaderObjects/glAttachObjectARB program-id fragment-shader-id)
+    (ARBShaderObjects/glLinkProgramARB program-id)
+    (ARBShaderObjects/glValidateProgramARB program-id)
+    (when (> (count (compile-errors program-id))
+             0)
+      (throw (Exception. (compile-errors program-id))))
+    program-id))
+
+(defn compile-program [vertex-shader-source fragment-shader-source]
+  (let [vertex-shader-id (create-vertex-shader)
+        fragment-shader-id (create-fragment-shader)]
+    (compile-shader vertex-shader-id vertex-shader-source)
+    (compile-shader fragment-shader-id fragment-shader-source)
+    (create-program vertex-shader-id
+                    fragment-shader-id)))
+
+(defn enable-program [program-id]
+  (ARBShaderObjects/glUseProgramObjectARB program-id))
+
+(defn disable-program []
+  (enable-program 0))
